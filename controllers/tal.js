30a20d064e73caf76bbee58beb980bb1d452f631
prasetiodc <mprasetiodc@gmail.com>
2020-03-31 16:52:36 +0700

    handle case update time tal

--- controllers/tal.js
+++ controllers/tal.js
@@ -248,6 +248,14 @@ class tal {
             score_tal: newScore
           }
 
+          if (Number(req.body.forDay) === 1) {
+            newData.when_day = req.body.time
+            newData.when_date = ''
+          } else {
+            newData.when_day = ''
+            newData.when_date = req.body.time
+          }
+
           let updateTAL = await tbl_tal_scores.update(newData, { where: { tal_score_id: req.params.id } })
 
           if (updateTAL && (req.body.achievement || req.body.weight)) {
