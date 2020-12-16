c417b7275a3d6f167d68309fccfd497f6f17f6be
prasetiodc <mprasetiodc@gmail.com>
2020-12-16 14:49:42 +0700

    update in app.js

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.use(express.json())
 app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
 app.use(morgan('dev'))
 app.use('/', route)
 
