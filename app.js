d7c0f943dccfdebb1dba5558a6ca21ebdabb19ae
prasetiodc <mprasetiodc@gmail.com>
2020-12-16 15:09:32 +0700

    update in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.use((req, res, next) => {
