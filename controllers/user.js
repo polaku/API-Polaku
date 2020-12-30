47e221b01d6f6547982f0248aee3b1f9ee56f616
prasetiodc <mprasetiodc@gmail.com>
2020-12-30 10:24:48 +0700

    uncomment filter company in user for option

--- controllers/user.js
+++ controllers/user.js
@@ -1537,22 +1537,22 @@ class user {
   static async forOption(req, res) {
     let condition = {}
 
-    // if (req.user.user_id !== 1) {
-    //   let userAdmin = await tbl_admin_companies.findAll({ where: { user_id: req.user.user_id } })
+    if (req.user.user_id !== 1) {
+      let userAdmin = await tbl_admin_companies.findAll({ where: { user_id: req.user.user_id } })
 
-    //   let tempCondition = [], idCompany = []
+      let tempCondition = [], idCompany = []
 
-    //   userAdmin && userAdmin.forEach(el => {
-    //     if (idCompany.indexOf(el.company_id) === -1) {
-    //       idCompany.push(el.company_id)
-    //       tempCondition.push({
-    //         company_id: el.company_id,
-    //       })
-    //     }
-    //   })
+      userAdmin && userAdmin.forEach(el => {
+        if (idCompany.indexOf(el.company_id) === -1) {
+          idCompany.push(el.company_id)
+          tempCondition.push({
+            company_id: el.company_id,
+          })
+        }
+      })
 
-    //   condition = { [Op.or]: tempCondition }
-    // }
+      condition = { [Op.or]: tempCondition }
+    }
 
     // condition[Op.or] = {
     //   status_employee: { [Op.ne]: 'Berhenti' },
