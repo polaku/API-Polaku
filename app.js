709dfc736d5aa7873da4a9729444810e6baff469
prasetiodc <mprasetiodc@gmail.com>
2021-02-22 14:32:25 +0700

    comment some line in app.js

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.use('/uploads', express.static('uploads'))
 app.use('/asset/img', express.static('assets'));
 app.use(morgan('dev'))
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-})
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// })
 
 app.use('/', route)
 
